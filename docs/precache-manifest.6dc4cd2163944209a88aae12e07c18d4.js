75f7d2c1cdcb8f0ca3a4076f2136f8fae5793604
Ezequiel <ezequielrebasa@gmail.com>
2020-09-12 20:01:46 -0300

    changing name-folder build to docs

---
+++ docs/precache-manifest.6dc4cd2163944209a88aae12e07c18d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84d7f86bd863293e14b1444c4bd3651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c813bca29ed9e0509d32",
+    "url": "/static/css/main.fe8a722c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c813bca29ed9e0509d32",
+    "url": "/static/js/main.61862073.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
